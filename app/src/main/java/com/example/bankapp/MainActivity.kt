f747479df12b4c545f5f4fe8f839628ba90404aa
Sarwat Aijaz <sarwataijaaz@gmail.com>
2024-04-06 17:33:29 +0500

    login page

--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.bankapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_main)
     }
 }
