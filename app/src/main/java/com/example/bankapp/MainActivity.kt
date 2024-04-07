12abf208112ae21155036040215a993662d5237c
Sarwat Aijaz <sarwataijaaz@gmail.com>
2024-04-07 07:24:29 +0500

    login and signup

--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.bankapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var signup: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
         setContentView(R.layout.activity_main)
+        signup = findViewById(R.id.signup)
+
+        signup.setOnClickListener {
+            Intent(this,SignUp::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/bankapp/SplashActivity.kt
+++ app/src/main/java/com/example/bankapp/SplashActivity.kt
@@ -1,34 +1,44 @@
 package com.example.bankapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.animation.Animation
+import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.ActivityOptionsCompat
 
 class SplashActivity : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         val handler = Handler(Looper.getMainLooper())
 
-        handler.postDelayed({
-            // Code to be executed after the delay
-           Intent(this,MainActivity::class.java).also {
-               startActivity(it)
-               finish() // remove from the activity stack
-           }
-        }, 3000) // Delay in milliseconds
         val topAnim = AnimationUtils.loadAnimation(this,R.anim.top_anim)
         val bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim)
         val logo: ImageView = findViewById(R.id.logo)
         val welcome: TextView = findViewById(R.id.welcome)
-        logo.startAnimation(bottomAnim)
-        welcome.startAnimation(topAnim)
+        logo.startAnimation(topAnim)
+        welcome.startAnimation(bottomAnim)
+
+        handler.postDelayed({
+            // Code to be executed after the delay
+           Intent(this,MainActivity::class.java).also {
+               val pairs = arrayOf(
+                   androidx.core.util.Pair<View, String>(logo, "logo_image"),
+                   androidx.core.util.Pair<View, String>(welcome, "logo_text")
+               )
+               val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, *pairs)
+                          startActivity(it,options.toBundle())
+                          finish() // remove from the activity stack
+           }
+        }, 3000) // Delay in milliseconds
+
     }
 }
