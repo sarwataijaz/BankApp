554837c2e3f664e8ace22936e0f6d49b5d758617
Sarwat Aijaz <sarwataijaaz@gmail.com>
2024-04-08 16:44:09 +0500

    login validation

--- app/src/main/java/com/example/bankapp/DB_Schema.kt
+++ app/src/main/java/com/example/bankapp/DB_Schema.kt
@@ -1,8 +1,11 @@
 package com.example.bankapp
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
 
 class DB_Schema(context : Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
 
@@ -13,12 +16,12 @@ class DB_Schema(context : Context) : SQLiteOpenHelper(context, DB_NAME, null, DB
         // Customer table
         private const val TABLE_CUSTOMER = "customer"
         private const val CUSTOMER_ID = "customer_id"
-        private const val DEBIT_CARD_NO = "debit_card_no"
         private const val ATM_PIN = "atm_pin"
         private const val CNIC = "cnic"
         private const val ACC_NO = "acc_no"
         private const val CUSTOMER_MONEY = "money"
         private const val CUSTOMER_NAME = "customer_name"
+        private const val PASSWORD = "password"
 
         // Beneficiary table
         private const val TABLE_BENEFICIARY = "beneficiary"
@@ -34,7 +37,7 @@ class DB_Schema(context : Context) : SQLiteOpenHelper(context, DB_NAME, null, DB
         val query1 = "CREATE TABLE $TABLE_CUSTOMER ( " +
                      "$CUSTOMER_ID INTEGER PRIMARY KEY, " +
                      "$CUSTOMER_NAME TEXT, " +
-                     "$DEBIT_CARD_NO INTEGER, " +
+                     "$PASSWORD INTEGER UNIQUE, " +
                      "$ATM_PIN INTEGER UNIQUE, " +
                      "$ACC_NO INTEGER UNIQUE, " +
                      "$CUSTOMER_MONEY INTEGER, " +
@@ -45,11 +48,47 @@ class DB_Schema(context : Context) : SQLiteOpenHelper(context, DB_NAME, null, DB
                 "$BENEFICIARY_NAME TEXT, " +
                 "$BENEFICIARY_ACC_NO INTEGER UNIQUE, " +
                 "$BENEFICIARY_MONEY INTEGER, " +
+                "$CUSTOMER_ID_FOREIGN_KEY INTEGER, " +
                 "FOREIGN KEY($CUSTOMER_ID_FOREIGN_KEY) REFERENCES $TABLE_CUSTOMER ($CUSTOMER_ID) )"
 
         db?.execSQL(query1)
         db?.execSQL(query2)
     }
+
+    fun addCustomerDetails(name: String, pass: String, pin: Int, cnic: Int, accNo: Int): Boolean {
+
+        val db : SQLiteDatabase = this.writableDatabase
+        val initialFunds = 100 // initial amount to award users
+
+        val values = ContentValues()
+        values.put(CUSTOMER_NAME,name)
+        values.put(PASSWORD,pass)
+        values.put(ATM_PIN,pin)
+        values.put(ACC_NO,accNo)
+        values.put(CUSTOMER_MONEY, initialFunds)
+        values.put(CNIC,cnic)
+
+        val newRowID = db.insert(TABLE_CUSTOMER, null, values)
+        db.close()
+
+        return (newRowID > -1)
+    }
+
+    fun loginValidity(pass: String) : Boolean {
+
+        val db = this.readableDatabase
+
+        val query = "SELECT * FROM $TABLE_CUSTOMER WHERE $PASSWORD = ?"
+        val selectionArgs = arrayOf(pass)
+
+        val cursor = db.rawQuery(query,selectionArgs)
+
+        val isValid = cursor.moveToFirst()
+        cursor.close()
+
+        return isValid
+    }
+
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         db?.execSQL("DROP TABLE IF EXISTS $TABLE_CUSTOMER")
--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -4,19 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var signup: Button
+    private lateinit var username: TextInputLayout
+    private lateinit var password: TextInputLayout
+    private lateinit var login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         signup = findViewById(R.id.signup)
+        login = findViewById(R.id.login)
 
         signup.setOnClickListener {
             Intent(this,SignUpActivity::class.java).also {
                 startActivity(it)
             }
+        }
+
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+
+        login.setOnClickListener {
+            val enteredPassword = password.editText?.text.toString()
+            val db = DB_Schema(this)
+            val isValid = db.loginValidity(enteredPassword)
+
+            if(isValid) {
+                Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Invalid!", Toast.LENGTH_SHORT).show()
+            }
+
+            db.close() // close the database connection
         }
     }
 }
